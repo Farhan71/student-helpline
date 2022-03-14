f96f018897e1dfe903ce323ebd57ff05ccabe1c5
Farhan71 <farhanitnstu@gmail.com>
2022-03-14 22:09:59 +0600

    my first commit6

--- routes/auth.js
+++ routes/auth.js
@@ -70,7 +70,7 @@ router.post("/login", async (req, res) => {
     res.status(200).json(others);
     // res.status(200).send({ data: token, message: "logged in successfully" });
   } catch (err) {
-    res.status(500).json(err);
+    // res.status(500).json(err);
   }
 });
 
